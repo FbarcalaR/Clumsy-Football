e37f6bb24c4ba8738dd53aee5fd557058070d567
Fernando <fernandobarcalarodriguez@gmail.com>
2020-09-28 20:39:55 +0200

    fix screen width

---
+++ Clumsy Football/Assets/Scripts/CameraCustom.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraCustom : MonoBehaviour
+{
+    public SpriteRenderer spriteToBeSeen;
+
+    public void Start()
+    {
+        float widthToBeSeen = spriteToBeSeen.bounds.size.x;
+        Camera.main.orthographicSize = widthToBeSeen * Screen.height / Screen.width * 0.5f;
+    }
+}
